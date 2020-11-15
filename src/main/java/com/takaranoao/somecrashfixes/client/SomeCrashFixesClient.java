3b6102e303417537d731fb780004bc39819d0d67
Takaranoao <baolan.zifu@gmail.com>
2020-11-15 09:08:50 +0800

    bug fix

--- src/main/java/com/takaranoao/somecrashfixes/client/SomeCrashFixesClient.java
+++ src/main/java/com/takaranoao/somecrashfixes/client/SomeCrashFixesClient.java
@@ -1,5 +1,14 @@
 package com.takaranoao.somecrashfixes.client;
 
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class SomeCrashFixesClient {
+public class SomeCrashFixesClient implements ClientModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger();
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
